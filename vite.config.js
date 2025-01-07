00e8a7ed8699553fa851868ef500869d85a700aa
Naod <chris_naod@yahoo.com>
2025-01-08 00:41:50 +1100

    added vercel and vite configuration

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        auth: resolve(__dirname, 'src/pages/auth.html'),
+        welcome: resolve(__dirname, 'src/pages/welcome.html'),
+      },
+    },
+  },
+});
